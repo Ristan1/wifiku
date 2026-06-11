db647d11195aa14161f9dce07b0e71df0c12d9a6
Ristan1 <100549107+Ristan1@users.noreply.github.com>
2026-06-11 14:47:35 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'wifipay-v1';
+const CACHE_NAME = 'wifipay-v2'; // Ubah versi ke v2 agar HP mereset cache
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
+  './',
+  './index.html',
+  './manifest.json',
   'https://cdn.tailwindcss.com',
   'https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js',
   'https://unpkg.com/@phosphor-icons/web'
@@ -26,6 +26,21 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+// Menghapus cache lama jika ada pembaruan versi
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
